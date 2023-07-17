f07eb3c7b176e36389928a9096a367aff012e945
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2023-07-17 16:44:49 +0700

    Create Web API Project

--- eShopSolution.Application/Catalog/Products/PublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/PublicProductService.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using eShopSolution.ViewModels.Catalog.Products;
 using eShopSolution.ViewModels.Common;
+using Azure.Core;
 
 namespace eShopSolution.Application.Catalog.Products
 {
@@ -18,6 +19,43 @@ namespace eShopSolution.Application.Catalog.Products
         public PublicProductService(EShopDbContext context)
         {
             _context = context;
+        }
+
+        public async Task<PagedResult<ProductViewModel>> GetAll()
+        {
+            // Select join
+            var query = from p in _context.Products
+                        join pt in _context.ProductTranslations on p.Id equals pt.ProductId
+                        join pic in _context.ProductInCategories on p.Id equals pic.ProductId
+                        join c in _context.Categories on pic.CategoryId equals c.Id
+                        select new { p, pt, pic };
+
+            // Paging
+            int totalRow = await query.CountAsync();
+
+            var data = await query.Select(x => new ProductViewModel()
+            {
+                Id = x.p.Id,
+                Name = x.pt.Name,
+                DateCreated = x.p.DateCreated,
+                Description = x.pt.Description,
+                Details = x.pt.Details,
+                LanguageId = x.pt.LanguageId,
+                OriginalPrice = x.p.OriginalPrice,
+                Price = x.p.Price,
+                SeoAlias = x.pt.SeoAlias,
+                SeoDescription = x.pt.SeoDescription,
+                SeoTitle = x.pt.SeoTitle,
+                Stock = x.p.Stock,
+                ViewCount = x.p.ViewCount
+            }).ToListAsync();
+
+            var pagedResult = new PagedResult<ProductViewModel>()
+            {
+                TotalRecord = totalRow,
+                Items = data
+            };
+            return await Task.FromResult(pagedResult);
         }
 
         public async Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetPublicProductPagingRequest request)
