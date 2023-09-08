b00e1873d9da2808bdb9d49c91b12274acc33d21
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2023-09-08 22:42:43 +0700

    Checkout cart

---
+++ eShopSolution.ApiIntegration/OrderApiClient.cs
@@ -0,0 +1,27 @@
+﻿using eShopSolution.ApiIntegration.Services;
+using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.Sales;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.ApiIntegration
+{
+	public class OrderApiClient : BaseApiClient, IOrderApiClient
+	{
+		public OrderApiClient(IHttpContextAccessor httpContextAccessor,
+							IHttpClientFactory httpClientFactory,
+							IConfiguration configuration) : base(httpContextAccessor, httpClientFactory, configuration)
+		{
+		}
+		public async Task<ApiResult<bool>> CreateOrder(CheckoutRequest request)
+		{
+			var result = await PostAsync<bool, CheckoutRequest>("/api/orders", request);
+			return result;
+		}
+	}
+}
---
+++ eShopSolution.WebApp/Models/CheckoutViewModel.cs
@@ -0,0 +1,10 @@
+﻿using eShopSolution.ViewModels.Sales;
+
+namespace eShopSolution.WebApp.Models
+{
+	public class CheckoutViewModel
+	{
+		public List<CartItemViewModel> CartItems { get; set; }
+		public CheckoutRequest CheckoutRequest { get; set; }
+	}
+}
